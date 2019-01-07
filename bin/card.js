5cb5681cee8b095f4d666ad6ae7c85c40bc1e205
Thanh Tran <trongthanh@gmail.com>
2019-01-07 12:17:17 +0700

    allow bin/

---
+++ bin/card.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+console.log('[32m[39m\n\
+[32m   ╭───────────────────────────────────────────────────────────╮[39m\n\
+[32m   │                                                           │[39m\n\
+[32m   │   [37m           Tran Trong Thanh[32m / [37mtrongthanh[32m                │[39m\n\
+[32m   │                                                           │[39m\n\
+[32m   │   [37m[1m   Title:[22m[32m  [37mFront-End Architect / JavaScript Developer[32m   │[39m\n\
+[32m   │   [37m[1m    From:[22m[32m  [37mHo Chi Minh city, Vietnam[32m                    │[39m\n\
+[32m   │                                                           │[39m\n\
+[32m   │   [37m[1m Twitter:[22m[32m  [90mhttps://twitter.com/[32m[36mtrongthanh[32m               │[39m\n\
+[32m   │   [37m[1m     npm:[22m[32m  [90mhttps://npmjs.com/[32m[31m~trongthanh[32m                │[39m\n\
+[32m   │   [37m[1m  GitHub:[22m[32m  [90mhttps://github.com/[32m[32mtrongthanh[32m                │[39m\n\
+[32m   │   [37m[1mLinkedIn:[22m[32m  [90mhttps://linkedin.com/in/[32m[94mtrongthanh[32m           │[39m\n\
+[32m   │   [37m[1mFacebook:[22m[32m  [90mhttps://facebook.com/[32m[34mtrongthanh[32m              │[39m\n\
+[32m   │   [37m[1m     Web:[22m[32m  [94mhttps://int3ractive.com[32m                      │[39m\n\
+[32m   │                                                           │[39m\n\
+[32m   │   [37m[1m    Card:[22m[32m  [31mnpx[32m [37mtrongthanh[32m                               │[39m\n\
+[32m   │                                                           │[39m\n\
+[32m   │                                                           │[39m\n\
+[32m   ╰───────────────────────────────────────────────────────────╯[39m\n\
+[32m[39m');
