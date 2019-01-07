8bf5d91247bb8889e2c3d0f18890c947e9ac4029
Thanh Tran <trongthanh@gmail.com>
2019-01-07 12:09:46 +0700

    initial commit, add my npx card implementation

---
+++ README.md
@@ -0,0 +1,15 @@
+# My NPM Card
+
+Based on @bnb's https://github.com/bnb/bitandbang
+
+I have updated to remove the file system read. One JS output is enough.
+
+# Usage
+
+Via npx:
+
+```
+npx trongthanh
+```
+
+
---
+++ build.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// Pull in our modules
+const chalk = require('chalk');
+const boxen = require('boxen');
+const fs = require('fs');
+const path = require('path');
+
+// Define options for Boxen
+const options = {
+	padding: 1,
+	margin: 1,
+	borderStyle: 'round',
+};
+
+// Text + chalk definitions
+// prettier-ignore
+const data = {
+	name:            chalk.white('           Tran Trong Thanh'),
+	handle:          chalk.white('trongthanh'),
+	work:            chalk.white('Front-End Architect / JavaScript Developer'),
+	from:            chalk.white('Ho Chi Minh city, Vietnam'),
+	twitter:         chalk.gray('https://twitter.com/') + chalk.cyan('trongthanh'),
+	npm:             chalk.gray('https://npmjs.com/') + chalk.red('~trongthanh'),
+	github:          chalk.gray('https://github.com/') + chalk.green('trongthanh'),
+	linkedin:        chalk.gray('https://linkedin.com/in/') + chalk.blueBright('trongthanh'),
+	facebook:        chalk.gray('https://facebook.com/') + chalk.blue('trongthanh'),
+	web:             chalk.blueBright('https://int3ractive.com'),
+	npx:             chalk.red('npx') + ' ' + chalk.white('trongthanh'),
+	labelWork:       chalk.white.bold('   Title:'),
+	labelFrom:       chalk.white.bold('    From:'),
+	labelTwitter:    chalk.white.bold(' Twitter:'),
+	labelnpm:        chalk.white.bold('     npm:'),
+	labelGitHub:     chalk.white.bold('  GitHub:'),
+	labelLinkedIn:   chalk.white.bold('LinkedIn:'),
+	labelFacebook:   chalk.white.bold('Facebook:'),
+	labelWeb:        chalk.white.bold('     Web:'),
+	labelCard:       chalk.white.bold('    Card:'),
+};
+
+// Actual strings we're going to output, newlines matter
+const output = `${data.name} / ${data.handle}
+
+${data.labelWork}  ${data.work}
+${data.labelFrom}  ${data.from}
+
+${data.labelTwitter}  ${data.twitter}
+${data.labelnpm}  ${data.npm}
+${data.labelGitHub}  ${data.github}
+${data.labelLinkedIn}  ${data.linkedin}
+${data.labelFacebook}  ${data.facebook}
+${data.labelWeb}  ${data.web}
+
+${data.labelCard}  ${data.npx}
+`;
+
+// frame the text
+const box = chalk.green(boxen(output, options));
+
+// generate the single js file that need run with npx
+fs.writeFileSync(
+	path.join(__dirname, 'bin/card.js'),
+	`#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+console.log('${box.split('\n').join('\\n\\\n')}');
+`
+);
